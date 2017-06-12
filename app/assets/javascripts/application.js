3b8ad75df30a2fa9a712097d282a0fe435018ebd
Christina Gyles <christinagyles@Christinas-MacBook-Pro.local>
2017-06-12 15:17:36 +0100

    fat fingers design test page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,21 @@ $(document).ready(function () {
   var showHideContent = new GOVUK.ShowHideContent()
   showHideContent.init()
 })
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var white_panel = this.nextElementSibling;
+        if (white_panel.style.display === "block") {
+            white_panel.style.display = "none";
+        } else {
+            white_panel.style.display = "block";
+        }
+    }
+}
