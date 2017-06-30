9eaea1ef4528ad8e6b9f83959f2c651fc2cb6710
Christina Gyles <c.gyles@live.co.uk>
2017-06-30 10:13:25 +0100

    new prototype v4 and changelog

--- app/routes.js
+++ app/routes.js
@@ -38,4 +38,66 @@ router.get('/v3/a/int/online_licence/map_of_abstraction_point', function (req, r
   })
 })
 
+router.get('/v4/int/online_licence', function (req, res) {
+  res.render('v4/int/online_licence',{
+     "chosenPermitID":req.query['wid']
+  })
+})
+
+module.exports = router
+
+router.get('/v4/int/online_licence/contact_details', function (req, res) {
+  res.render('v4/int/online_licence/contact_details',{
+     "chosenPermitID":req.query['wid']
+  })
+})
+
+module.exports = router
+
+router.get('/v4/int/online_licence/terms', function (req, res) {
+  res.render('v4/int/online_licence/terms',{
+     "chosenPermitID":req.query['wid']
+  })
+})
+
+module.exports = router
+
+router.get('/v4/int/online_licence/map_of_abstraction_point', function (req, res) {
+  res.render('v4/int/online_licence/map_of_abstraction_point',{
+     "chosenPermitID":req.query['wid']
+  })
+})
+
+module.exports = router
+
+router.get('/v4/ext/online_licence', function (req, res) {
+  res.render('v4/ext/online_licence',{
+     "chosenPermitID":req.query['wid']
+  })
+})
+
+module.exports = router
+
+router.get('/v4/ext/online_licence/contact_details', function (req, res) {
+  res.render('v4/ext/online_licence/contact_details',{
+     "chosenPermitID":req.query['wid']
+  })
+})
+
+module.exports = router
+
+router.get('/v4/ext/online_licence/terms', function (req, res) {
+  res.render('v4/ext/online_licence/terms',{
+     "chosenPermitID":req.query['wid']
+  })
+})
+
+module.exports = router
+
+router.get('/v4/ext/online_licence/map_of_abstraction_point', function (req, res) {
+  res.render('v4/ext/online_licence/map_of_abstraction_point',{
+     "chosenPermitID":req.query['wid']
+  })
+})
+
 module.exports = router
