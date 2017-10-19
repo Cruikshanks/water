2a17eb4c8511b28ff81ddec4efd488954869b1d3
Christina Gyles <christinagyles@Christinas-MacBook-Pro.local>
2017-10-19 10:26:18 +0100

    removed name from v9 licences, updated service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Water resource licensing service',
+  serviceName: 'Manage your water abstraction or impoundment licence',
 
   // Default port that prototype runs on
   port: '3000',
