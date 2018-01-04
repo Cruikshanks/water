49aea1295f16e1fcbdaf65215c98c6110f735051
Christina Gyles <christinagyles@Christinas-MacBook-Pro.local>
2018-01-04 13:58:19 +0000

    version 12 create and abstraction condition pages and contact details request at login

--- app/assets/javascripts/license_results.js
+++ app/assets/javascripts/license_results.js
@@ -36,7 +36,7 @@ $(function() {
 
     // For prototype, just load one of three pages at random
     var page = Math.ceil(Math.random() * 3);
-    var url = '/v11/online_licence/search_results/licence_holder_search_results_' + page;
+    var url = '/v12/online_licence/search_results/licence_holder_search_results_' + page;
 
     $.get(url, function(html) {
       $('#results').append(html);
