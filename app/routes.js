637d3cac86f680dea86363f64be03845ee1a02c8
Steve Murphy <steve.murphy.ea@gmail.com>
2018-01-21 20:24:08 +0000

    Added content links for version 13

--- app/routes.js
+++ app/routes.js
@@ -3103,3 +3103,21 @@ router.get('/v13/pages/point_purpose', function (req, res) {
     "chosenPermitID":req.query['wid']
  })
 })
+
+
+module.exports = router
+
+router.get('/v13/pages/online_licence', function (req, res) {
+ res.render('v13/pages/online_licence',{
+    "chosenPermitID":req.query['wid']
+ })
+})
+
+
+module.exports = router
+
+router.get('/v13/pages/variation_summary', function (req, res) {
+ res.render('v13/pages/variation_summary',{
+    "chosenPermitID":req.query['wid']
+ })
+})
