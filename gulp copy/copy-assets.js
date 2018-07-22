bdca7ce4357ee8cf2d176574f0149f4207f3a123
Steve Murphy <steve.murphy.ea@gmail.com>
2018-07-22 15:12:28 +0100

    proto gulp fix

--- gulp copy/copy-assets.js
+++ gulp copy/copy-assets.js
@@ -10,17 +10,17 @@ const config = require('./config.json')
 gulp.task('copy-assets', function () {
   return gulp.src(['!' + config.paths.assets + 'sass{,/**/*}',
     config.paths.assets + '/**'])
-    .pipe(gulp.dest(config.paths.public))
+  .pipe(gulp.dest(config.paths.public))
 })
 
-gulp.task('copy-assets-documentation', function () {
+gulp.task('copy-documentation-assets', function () {
   return gulp.src(['!' + config.paths.docsAssets + 'sass{,/**/*}',
     config.paths.docsAssets + '/**'])
-    .pipe(gulp.dest(config.paths.public))
+  .pipe(gulp.dest(config.paths.public))
 })
 
-gulp.task('copy-assets-v6', function () {
-  return gulp.src(['!' + config.paths.v6Assets + 'sass{,/**/*}',
-    config.paths.v6Assets + '/**'])
-    .pipe(gulp.dest(config.paths.public + '/v6'))
+gulp.task('copy-v6-assets', function () {
+  return gulp.src(['!app/v6/assets/sass{,/**/*}',
+    'app/v6/assets' + '/**'])
+  .pipe(gulp.dest(config.paths.public + '/v6'))
 })
